4f0223231c83023e867ac2121c9241eec49547f5
jonaselmesten <jonas_elme@hotmail.com>
2020-10-09 10:38:20 +0200

    Added feature - GPS metadata data extraction. And overall cleanup of the code.

--- deepforest/__init__.py
+++ deepforest/__init__.py
@@ -7,7 +7,36 @@ __version__ = '0.3.2'
 import os
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
+print("ROOT:", _ROOT)
 
 
 def get_data(path):
-    return os.path.join(_ROOT, 'data', path)
+    return os.path.join(_ROOT, 'deepforest/data', path)
+
+
+def get_eval(path):
+    return os.path.join(_ROOT, "\\model\\eval", path)
+
+
+def get_train(path):
+    return os.path.join(_ROOT, "\\model\\train", path)
+
+
+def get_train_dir():
+    return os.path.join(_ROOT, "\\model\\train")
+
+
+def get_output(path):
+    return os.path.join(_ROOT, "output", path)
+
+
+def get_output_dir():
+    return os.path.join(_ROOT, "output")
+
+
+def get_model(path):
+    return os.path.join(_ROOT, "model", path)
+
+
+def get_model_dir():
+    return os.path.join(_ROOT, "model")
