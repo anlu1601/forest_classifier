f02be26e27216091a5b10c8dc916468485a9bc06
Carriewenyangyang <45647156+Carriewenyangyang@users.noreply.github.com>
2020-10-24 23:11:51 +0200

    Add GN_driver.py
    The main function and functions in other files can be called here.

---
+++ GN_driver.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import Divide_DataSet as dv
+from GN import create_model
+from GN import train_model
+from GN import predict
+import os
+
+
+def divideDataset(original_folder, output_folder):
+    dv.divide(original_folder, output_folder)
+    train_root = output_folder + '/train/'
+    validation_root = output_folder + '/val/'
+    test_root= output_folder + '/test/'
+    return train_root, validation_root, test_root
+    
+def check_model(folder, model, train_root, validation_root, test_root):
+    if os.path.exists(model):
+        print("It exists. Predicting...")
+        name, results = predict(folder, model)
+#         print("images_names",name)
+#         print("predictions",results)
+    else:
+        print("Model doesn't exist. Creating a new one...")
+        model = create_model()
+        trained_model = train_model(model, train_root, validation_root, test_root)
+        name, results = predict(folder, trained_model)
+    return name, results
+
+
+# In[5]:
+
+
+if __name__ == "__main__":
+    model = 'inception_1.h5'
+    to_be_predict_folder = 'unknown_images'
+     # step1: create trainset, validationset, testingset
+    (train_root, validation_root, test_root) = divideDataset("dataset",  "after_divide_dataset") 
+    # step2: build and predict
+    (name, results) = check_model(to_be_predict_folder, model, train_root, validation_root, test_root)
+
+
+# In[ ]:
+
+
+
+
