e21597cb8662493400d18908ae6a6e94e46d8587
Carriewenyangyang <45647156+Carriewenyangyang@users.noreply.github.com>
2020-10-24 23:05:49 +0200

    Update Divide_DataSet.py

--- Divide_DataSet.py
+++ Divide_DataSet.py
@@ -5,7 +5,11 @@
 
 
 import splitfolders
-splitfolders.ratio("dataset", output="after_divide_dataset/", seed=1337, ratio=(.8, .1, .1), group_prefix=None) # default values
+def divide(_input, _output):
+#     splitfolders.ratio("dataset", output="after_divide_dataset/", seed=1337, ratio=(.8, .1, .1), group_prefix=None) # default values
+    splitfolders.ratio(_input, output=_output + "/", seed=1337, ratio=(.8, .1, .1), group_prefix=None) # default values
+    print("Dividing images into folders.")
+    return
 
 
 # In[ ]:
