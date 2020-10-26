4b5f46a8627727976cfb85993a88055fb5715834
anlu1601 <anlu1601@student.miun.se>
2020-10-26 02:18:56 +0100

    Added environment.yml for conda installation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from clustering.functions import cluster_and_plot
 from folders import get_tso, get_tsd
-from gui.plot_image import plot_trees_and_select
 from tree_segmentation.tree_predictor import TreePredictor
 import interaction.user_interaction as interact
 import interaction.import_trees as imp_trees
@@ -12,9 +10,9 @@ import clustering.make_cluster as mc
 
 if __name__ == '__main__':
     # PREDICT AND SAVE TREES, SEGMENTATION
-    #predictor = TreePredictor()
-    #predictor.load_model('trained_model.h5')
-    #predictor.predict_and_store_trees_from_folder(folder_path="./tree_segmentation/data", save_json=True, save_folder=get_tso())
+    predictor = TreePredictor()
+    predictor.load_model('trained_model.h5')
+    predictor.predict_and_store_trees_from_folder(folder_path="./tree_segmentation/data", save_json=True, save_folder=get_tso())
 
     # CREATE CLUSTER AND SHOW USER
     data, forest_image = imp_trees.get_trees_from_random_forest()
